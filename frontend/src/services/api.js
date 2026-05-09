0d4444289b1fb15ee1bd6be453a522481464f343
gustavosena025-dotcom <gustavosena025@gmail.com>
2026-05-09 21:13:51 +0000

    feat: página Register com toast de sucesso e redirecionamento

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "http://localhost:3000/api", // ajuste conforme seu backend
 });
 
-export default api;
+export const cadastrarUsuario = async (dados) => {
+  try {
+    const response = await api.post("/usuarios/cadastro", dados);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || { message: "Erro inesperado" };
+  }
+};
